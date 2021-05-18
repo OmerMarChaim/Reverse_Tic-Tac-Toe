d9853c8a92fed2017bd35283ccb35fb519d88fe8
Omri Cohen <omricohen101@gmail.com>
2021-05-18 11:49:15 +0300

    Change the modifier of GameLogic.cs - Board,m_Player(S), m_CurrentGameState, m_WinnerPlayerto static. **MALIKIM**

--- ReverseTicTacToeGame/Board.cs
+++ ReverseTicTacToeGame/Board.cs
@@ -1,31 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Text;
-
+﻿using System.Collections.Generic;
 
 namespace ReverseTicTacToeGame
 {
     internal class Board
     {
-        private readonly int r_Size; 
+        private readonly int r_Size;
         private static eSignsOfPlayers[,] s_Board;
-     
-        HashSet<(int, int)> m_FreeSpots;
-        private const int k_MinBoardSize = 3;
-        private const int k_MaxBoardSize = 9;
-        internal const char k_Empty = (char)0;
-        private const char k_Circle = 'O';
-        private const char k_Cross = 'X';
-        private const int k_QSign = -1;
-  
 
+        HashSet<(int, int)> m_FreeSpots;
+        private const int k_QSign = -1;
 
         public Board(int i_Size)
         {
             r_Size = i_Size;
             s_Board = new eSignsOfPlayers[i_Size, i_Size];
-            m_FreeSpots=new HashSet<(int, int)>();
+            m_FreeSpots = new HashSet<(int, int)>();
             for(int i = 1; i < i_Size; i++)
             {
                 for(int j = 1; j < i_Size; j++)
@@ -33,8 +22,7 @@ namespace ReverseTicTacToeGame
                     m_FreeSpots.Add((i, j));
                     s_Board[i, j] = eSignsOfPlayers.Empty;
                 }
-            } 
-
+            }
         }
 
         public int Size
@@ -50,7 +38,6 @@ namespace ReverseTicTacToeGame
             get { return s_Board; }
             set { s_Board = value; }
         }
-     
 
         internal void SetValueOnBoard(int i_Row, int i_Column, eSignsOfPlayers i_Symbol)
         {
@@ -58,21 +45,19 @@ namespace ReverseTicTacToeGame
             {
                 s_Board[i_Row, i_Column] = i_Symbol;
                 m_FreeSpots.Remove((i_Row, i_Column));
-
             }
         }
 
         internal bool IsEmptySpot(int i_Row, int i_Column)
         {
             bool isEmptySpot = s_Board[i_Row, i_Column] == eSignsOfPlayers.Empty;
+
             return isEmptySpot;
         }
-
 
         internal bool IsFull()
         {
             return this.m_FreeSpots.Count == 0;
         }
-
     }
 }
